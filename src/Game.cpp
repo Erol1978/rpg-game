dc4ec4c1a868c2c6d97881d979161151eb3b3b9b
Erol1978 <30065105@cc.peralta.edu>
2026-04-17 02:56:21 +0000

    Added Game implementation skeleton

---
+++ src/Game.cpp
@@ -0,0 +1,23 @@
+#include "Game.h"
+
+Game::Game() : running(true) {
+}
+
+Game::~Game() {
+}
+
+void Game::startGame() {
+}
+
+void Game::processInput(char input) {
+}
+
+void Game::update() {
+}
+
+void Game::checkWinLose() const {
+}
+
+Player Game::getPlayer() const {
+    return player;
+}
